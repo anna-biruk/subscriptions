bb887b6823f3e559b7a9c1f9a2a82e6efb03cca3
Anna Biruk <49333189+anna-biruk@users.noreply.github.com>
2022-07-11 17:14:00 +0200

    added git ignore

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,25 +1,48 @@
 import {LockClosedIcon} from '@heroicons/react/solid'
+import {useDispatch, useSelector} from "react-redux";
+import {useState} from "react";
+import {Link, useNavigate} from "react-router-dom";
+import {login} from "../../redux/actions/user";
+import Spinner from "../Spinner/Spinner";
+import {selectIsLoading} from "../../redux/selectors";
 
 const LoginForm = () => {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const isLoading = useSelector(selectIsLoading)
+
+    const handleSubmitLoginForm = (e) => {
+        e.preventDefault()
+        dispatch(login(username, password)).then(({success}) => {
+            if (success) {
+                navigate('/list-items')
+            }
+        })
+    }
     return (
         <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
             <div className="max-w-md w-full space-y-8">
                 <h1 className="mb-8 text-3xl text-center">Sign in</h1>
-                <form className="mt-8 space-y-6" action="#" method="POST">
+                <form className="mt-8 space-y-6" action="#" method="POST" onSubmit={handleSubmitLoginForm}>
                     <input type="hidden" name="remember" defaultValue="true"/>
                     <div className="rounded-md shadow-sm -space-y-px">
                         <div>
                             <label htmlFor="email-address" className="sr-only">
-                                Email address
+                                Username
                             </label>
                             <input
-                                id="email-address"
-                                name="email"
-                                type="email"
-                                autoComplete="email"
+                                value={username}
+                                onChange={event => setUsername(event.target.value)}
+                                id="username"
+                                name="username"
+                                type="text"
+                                autoComplete="username"
                                 required
                                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                                placeholder="Email address"
+                                placeholder="Username"
                             />
                         </div>
                         <div>
@@ -27,6 +50,8 @@ const LoginForm = () => {
                                 Password
                             </label>
                             <input
+                                value={password}
+                                onChange={event => setPassword(event.target.value)}
                                 id="password"
                                 name="password"
                                 type="password"
@@ -52,9 +77,9 @@ const LoginForm = () => {
                         </div>
 
                         <div className="text-sm">
-                            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                                Forgot your password?
-                            </a>
+                            <Link to="/sign-up" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                Sign-up
+                            </Link>
                         </div>
                     </div>
 
@@ -63,10 +88,13 @@ const LoginForm = () => {
                             type="submit"
                             className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                         >
-                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                  <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true"/>
-                </span>
-                            Sign in
+                            {isLoading ? <Spinner/> : (<>
+                                    <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                                    <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                                                    aria-hidden="true"/>
+                                    </span>
+                                Sign in
+                            </>)}
                         </button>
                     </div>
                 </form>
